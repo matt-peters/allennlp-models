a93364ede6e900251082401cfa1af12b580e9f47
Matthew Peters <matthewp@allenai.org>
2020-10-23 12:55:25 -0700

    commonsense qa for roberta

--- allennlp_models/mc/dataset_readers/commonsenseqa.py
+++ allennlp_models/mc/dataset_readers/commonsenseqa.py
@@ -23,11 +23,15 @@ class CommonsenseQaReader(TransformerMCReader):
         logger.info("Reading file at %s", file_path)
         from allennlp.common.file_utils import json_lines_from_file
 
+        # see https://github.com/pytorch/fairseq/tree/master/examples/roberta/commonsense_qa
+        # uses <s> Q: Where would I not want a fox? </s> A: hen house </s> for format
         for json in json_lines_from_file(file_path):
-            choices = [(choice["label"], choice["text"]) for choice in json["question"]["choices"]]
+            choices = [(choice["label"], " A: " + choice["text"].strip()) for choice in json["question"]["choices"]]
             correct_choice = [
                 i for i, (label, _) in enumerate(choices) if label == json["answerKey"]
             ][0]
+            # ["question"]["stem"] is the question
+            question = " Q: " + json["question"]["stem"].strip()
             yield self.text_to_instance(
                 json["id"], json["question"]["stem"], [c[1] for c in choices], correct_choice
             )
